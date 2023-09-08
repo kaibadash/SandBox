adf2a0836ff021c6494e392f6f86d608e3c42b7d
kaiba <kaibadash@gmail.com>
2023-09-08 22:26:45 +0900

    Review

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -7,8 +7,10 @@ function fizzBuzz(target) {
       // 3と5の倍数のとき
       alert("FizzBuzz");
     } else if (i % 3 == 0) {
+      // の倍数のとき
       alert("Fizz");
     } else if (i % 5 == 0) {
+      // 5の倍数のとき
       alert("Buzz");
     } else {
       alert(i);
